26d13ab5e55b2ffc0f4b2f5e00928ca5780769e1
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-11-03 10:46:22 -0700

    New app.js initial commit, new album model and dependancies
    + remove old files

--- app.js
+++ app.js
@@ -1,94 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compression = require('compression')
-
+// const newrelic = require('newrelic')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const compression = require('compression')
 // security
-var helmet = require('helmet'); 
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
-// var csp = require('express-csp-header');
-var cors = require('cors');
+const helmet = require('helmet') 
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+const cors = require('cors')
 
-var index = require('./routes/index.route');
-var about = require('./routes/about.route')
-var favorites = require('./routes/favorites.route');
-var myfavorites = require('./routes/myfavorites.route')
-var search = require('./routes/search.route');
-var albumdetails = require('./routes/albumdetails.route');
-var update = require('./routes/update.route');
-var alltags = require('./routes/alltags.route');
-var apiRout = require('./routes/api.v1.route')
-
-// Connecting my database
-var mongo = require('mongodb');
-var monk = require('monk');
-// var db = monk('mongodb://joshua:roofuzz@ds263948.mlab.com:63948/music-tags');
-var db = monk('mongodb://joshua:roofuzz@album-tags-shard-00-00-qntxy.mongodb.net:27017,album-tags-shard-00-01-qntxy.mongodb.net:27017,album-tags-shard-00-02-qntxy.mongodb.net:27017/Album-Tags?ssl=true&replicaSet=Album-Tags-shard-0&authSource=admin&retryWrites=true');
-
-// Use this to see when the database is connected
-// db.then(() => {
-//   console.log('Connected correctly to database server')
-// })
-
-var app = express();
-
+// module.exports allows me to use app in tests
+const app = express()
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// security
-app.use(helmet());
-app.use(redirectToHTTPS([/localhost:(\d{4})/]));
-app.use(cors());
-
-
-// Making my DB accessable to the router
-app.use(function(req,res,next){
-	req.db = db;
-	next();
-});
-
-// compress all responses
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
 app.use(compression())
 
-app.use('/', index);
-app.use('/about', about);
-app.use('/favorites', favorites);
-app.use('/myfavorites', myfavorites);
-app.use('/search', search);
-app.use('/albumdetails', albumdetails);
-app.use('/update', update);
-app.use('/alltags', alltags);
-app.use('/api/v1', apiRout);
+// security
+app.use(helmet())
+// console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') { app.use(redirectToHTTPS([/localhost:(\d{4})/])) }
+app.use(cors())
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// ====== Set up database connection ======
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+mongoose.connect('mongodb://joshua:roofuzz@album-tags-shard-00-00-qntxy.mongodb.net:27017,album-tags-shard-00-01-qntxy.mongodb.net:27017,album-tags-shard-00-02-qntxy.mongodb.net:27017/Album-Tags?ssl=true&replicaSet=Album-Tags-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true })
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// ====== Set up routes ======
+const albumRoutes = require('./routes/album.routes')
+const appleAPIRoutes = require('./routes/appleAPI.routes')
+const staticRoutes = require('./routes/static.routes')
+app.use('/api/v1/album', albumRoutes)
+app.use('/api/v1/apple', appleAPIRoutes)
+app.use('/', staticRoutes)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  // console.log(`Albumtags is running on port ${PORT}.`)
+})
 
-module.exports = app;
+module.exports = app
---
+++ models/album.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// ====== Model Notes: ======
+
+let AlbumSchema = new Schema({
+  appleAlbumID: {type: String, required: true},
+  appleURL: {type: String, required: true},
+  title: {type: String, required: true},
+  titleKeyWords: {type: Array, required: true},
+  artist: {type: String, required: true},
+  artistKeyWords: {type: Array, required: true},
+  releaseDate: {type: String, required: false},
+  recordCompany: {type: String, required: false},
+  songNames: {type: Array, required: true},
+  cover: {type: String, required: true},
+  genres: {type: Array, required: false},
+  tagObjects: {type: Array, required: false},
+  tags: {type: Array, required: false},
+  connectionObjects: {type: Array, required: false}
+}, { collection : 'new-back-end-test' })
+
+module.exports = mongoose.model('Album', AlbumSchema)
