d06cca756c08effd576611e2e265bbaf3ab650c9
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-01-16 09:00:36 -0800

    Finish list UI
    Improve `get list`, add `delete album`, `edit display name` and `edit title`. Add functionality to prepare for handling shared favorites lists.

--- public/javascripts/list.script.js
+++ public/javascripts/list.script.js
@@ -14,18 +14,33 @@ function truncate(str, len){
 
 let listData
 const listID = window.location.pathname.replace('/list/', '')
+let isFavoritesList = false
 
-function getLists() {
-  $.ajax({
-    method: "GET",
-    // url: "/api/v1/list/user/" + userID,
-    url: "/api/v1/list/" + listID,
-    success: function(data) {
-      listData = data
-      $('#loader').hide()
-      populateList()
-    }
-  })
+function getList() {
+  if (listID.length < 25) {
+    $.ajax({
+      method: "GET",
+      // url: "/api/v1/list/user/" + userID,
+      url: "/api/v1/list/" + listID,
+      success: function(data) {
+        listData = data
+        $('#loader').hide()
+        populateList()
+      }
+    })
+  } else {
+    isFavoritesList = true
+    $.ajax({
+      method: "GET",
+      // url: "/api/v1/list/user/" + userID,
+      url: "/api/v1/list/favorites/" + listID,
+      success: function(data) {
+        listData = data
+        $('#loader').hide()
+        populateList()
+      }
+    })
+  }
 }
 
 function createCard(cardNumber) {
@@ -52,22 +67,122 @@ function populateCard(album, cardNumber) {
   $(`#card${cardNumber} img`).attr('src', album.cover.replace('{w}', 260).replace('{h}', 260))
   // add album-details-link to album cover
   $(`#card${cardNumber} .album_details_link`).attr('href', `/albumdetails/${album.appleAlbumID}`)
+  
+  $(`#card${cardNumber}`).append(`<span class="album-delete-button" data-album-id="${album.appleAlbumID}" data-toggle="tooltip" data-placement="right" title="Remove from list" data-trigger="hover">&#10005;</span></li>`)
+
+  // checks each time through in case login is slow
+  if (userID && !isFavoritesList) {
+    $('.album-delete-button').show()
+  } else {
+    $('.album-delete-button').hide()
+  }
 }
 
 function populateList() {
+  $("#no-albums-message").hide()
   $('#albums').html('')
   // console.log(listData)
   let listCreator = listData.displayName
-  if (listCreator.trim === "") { listCreator = "Unknown" }
+  if (!listCreator || listCreator.trim === "") { listCreator = "Unknown" }
   $('#list-title').text(`${listData.title}`)
   $('#list-creator').text("by: " + listCreator)
-  let card = 0
-  listData.albums.forEach(albumObject => {
-    card = card + 1
-    createCard(card)
-    populateCard(albumObject.album || albumObject, card)
+
+  if (listData.albums && listData.albums.length > 0) {
+    let card = 0
+    listData.albums.forEach(albumObject => {
+      card = card + 1
+      createCard(card)
+      populateCard(albumObject.album || albumObject, card)
+    })
+  
+    // ====== add event listener to delete buttons =====
+    $(".album-delete-button").on("click", function() { 
+      removeAlbum($(this).attr("data-album-id")) 
+    })
+  } else {
+    $("#no-albums-message").show()
+  }
+
+  if (isFavoritesList) { $('#edit-button').hide(); }
+}
+
+function removeAlbum(albumID) {
+  let thisAlbum = listData.albums.find(x => x.appleAlbumID === albumID)
+  let confirmed = confirm(`Are you sure you want to remove "${thisAlbum.title}" from this list? You cannot undo this operation.`)
+  
+  if (confirmed) {
+    let deleteObject = {
+      method: "remove album",
+      appleAlbumID: thisAlbum.appleAlbumID,
+      title: thisAlbum.title,
+      artist: thisAlbum.artist,
+      releaseDate: thisAlbum.releaseDate,
+      cover: thisAlbum.cover
+    }
+    
+    $.ajax({
+      method: "PUT",
+      url: "/api/v1/list/" + listID,
+      contentType: 'application/json',
+      data: JSON.stringify(deleteObject),
+      success: function(data) {
+        listData = data
+        populateList()
+      }
+    })
+  }
+}
+
+function editDisplayName() {
+  let newDisplayName = $("#list-display-name-input").val().trim()
+  let updateObject = {
+    method: "change display name",
+    displayName: newDisplayName
+  }
+  $.ajax({
+    method: "PUT",
+    url: "/api/v1/list/" + listID,
+    contentType: 'application/json',
+    data: JSON.stringify(updateObject),
+    success: function(data) {
+      listData = data
+      populateList()
+    }
   })
 }
+
+function editListTitle() {
+  let newListTitle = $("#list-title-input").val().trim()
+  if (newListTitle && newListTitle.length > 0) {
+    let updateObject = {
+      method: "change title",
+      title: newListTitle
+    }
+    $.ajax({
+      method: "PUT",
+      url: "/api/v1/list/" + listID,
+      contentType: 'application/json',
+      data: JSON.stringify(updateObject),
+      success: function(data) {
+        listData = data
+        populateList()
+      }
+    })
+  } else {
+    alert("A non-blank title is required for every list.")
+    $('#list-title-input').val(listData.title)
+  }
+}
+
+document.getElementById("edit-button").addEventListener("click", function() {
+  $('#editListModal').modal('show')
+  $('#list-title-input').val(listData.title)
+  $('#list-display-name-input').val(listData.displayName || "Unknown")
+})
+
+document.getElementById("update-list-display-name").addEventListener("click", editDisplayName)
+document.getElementById("update-list-title").addEventListener("click", editListTitle)
+
 // ----- START FIREBASE AUTH SECTION ------
 const config = {
   apiKey: "AIzaSyAoadL6l7wVMmMcjqqa09_ayEC8zwnTyrc",
@@ -75,33 +190,39 @@ const config = {
   projectId: "album-tags-v1d1",
 }
 const defaultApp = firebase.initializeApp(config)
-
+let userID = false
 // checking if user is logged in or logs in during session
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     userID = firebase.auth().currentUser.uid
-    getLists()
+    getList()
 
     $('#full_menu_login_logout_container').show()
     $('#login_button').hide()
     $('#full_menu_login_button').hide()
     $('#logout_button').show()
     $('#full_menu_logout_button').show()
-    $('#log_in_message').hide()
+
+    $('#edit-button').show()
+    $('.album-delete-button').show()
   } else {   
     // no user logged in
-    $('#all_cards').html('')
+    userID = false
+
+    getList()
+
     $('#full_menu_login_logout_container').show()
     $('#login_button').show()
     $('#full_menu_login_button').show()
     $('#logout_button').hide()
     $('#full_menu_logout_button').hide()
     $('#loader').hide()
-    $('#log_in_message').show()   
+
+    $('#edit-button').hide()
+    $('.album-delete-button').hide()
   }
 })
 
-let userID
 function logIn() {
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
   // local persistance remains if browser is closed
@@ -111,7 +232,7 @@ function logIn() {
   })
   .then(function(result) {
     userID = user.uid
-    getLists()
+    getList()
 
     $('#full_menu_login_logout_container').show()
     $('#login_button').hide()
@@ -119,6 +240,8 @@ function logIn() {
     $('#logout_button').show()
     $('#full_menu_logout_button').show()
 
+    $('#edit-button').show()
+    $('.album-delete-button').show()
   }).catch(function(error) {
     // Handle Errors here.
   })
@@ -126,6 +249,7 @@ function logIn() {
 
 function logOut() {
   firebase.auth().signOut().then(function() {
+    userID = false
     // log out functionality
     $('#full_menu_login_logout_container').show()
     $('#login_button').show()
@@ -133,6 +257,8 @@ function logOut() {
     $('#logout_button').hide()
     $('#full_menu_logout_button').hide()
 
+    $('#edit-button').hide()
+    $('.album-delete-button').hide()
   }).catch(function(error) {
   // An error happened.
   })
