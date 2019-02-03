99a434db1f493cc75424a3520893b476f9d63b67
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-02-02 16:59:42 -0800

    Start search modal on album page

---
+++ public/javascripts/search_modal.script.js
@@ -0,0 +1,77 @@
+// ------- START UTILITIES SECTION ----------
+function truncate(str, len){
+  // set up the substring
+  var subString = str.substr(0, len-1)
+  
+  return (
+    // add elipse after last complete word
+    subString.substr(0, subString.lastIndexOf(' '))
+    // trim trailing comma
+    .replace(/(^[,\s]+)|([,\s]+$)/g, '') + '...'
+  )
+}
+// ------- END UTILITIES SECTION ----------
+
+function executeSearch(searchString) {
+  $.ajax({
+    method: "GET",
+    url: "/api/v1/apple/search/" + searchString,
+    success: function(data) {
+      if (data.message) {
+        alert(data.messsage);
+        return;
+      } else {
+        populateSearchModalResults(data);
+      }
+    }
+  });
+}
+
+function populateSearchModalResults(data) {
+  $('#search-modal-results').html('');
+  for (let index = 0; index < data.albums.length; index++) {
+    const album = data.albums[index];
+    const cardNumber = index + 1;
+    createModalCard(cardNumber);
+    populateModalCard(album, cardNumber);
+  }
+}
+
+function createModalCard(cardNumber) {
+  $('#search-modal-results').append(`<div id="searchModalCard${cardNumber}" class="search-modal-card"><a class="search-modal-card-album-link" href=""><img class="search-modal-card-image" src="" alt=""><a/><div class="search-modal-card-body"><h4 class="search-modal-card-title"></h4><span class="search-modal-card-album"></span></div></div>`)
+}
+
+function populateModalCard(album, cardNumber) {
+  // set up album and artist trunction
+  let smallArtist = album.artist
+  let largeArtist = album.artist
+  let smallAlbum = album.title
+  let largeAlbum = album.title
+  if (smallArtist.length > 32) { smallArtist = truncate(smallArtist, 32) } 
+  if (smallAlbum.length > 44) { smallAlbum = truncate(smallAlbum, 44) } 
+
+  if (largeArtist.length > 49) { largeArtist = truncate(largeArtist, 49) } 
+  if (largeAlbum.length > 66) { largeAlbum = truncate(largeAlbum, 66) }
+  
+  // artist name
+  $(`#searchModalCard${cardNumber} .search-modal-card-title`).html(`<span class="search-modal-card-large-artist">${largeArtist}</span><span class="search-modal-card-small-artist">${smallArtist}</span>`)
+  // album name
+  $(`#searchModalCard${cardNumber} .search-modal-card-album`).html(`<span class="search-modal-card-large-album">${largeAlbum}</span><span class="search-modal-card-small-album">${smallAlbum}</span>`) 
+  // album cover
+  $(`#searchModalCard${cardNumber} .search-modal-card-image`).attr('src', album.cover.replace('{w}', 260).replace('{h}', 260))
+  // add album-details-link to album cover
+  $(`#searchModalCard${cardNumber} .search-modal-card-album-link`).attr('href', `/album/${album.appleAlbumID}`)
+}
+
+$('#search-modal-button').click(function(event) {
+  event.preventDefault();
+  const search = $('#search-modal-input').val().trim().replace(/[^\w\s]/gi, '');
+  executeSearch(search);
+});
+
+// execute search on enter key
+$("#search-modal-input").keyup(function(event) {
+  if (event.keyCode === 13) {
+    $("#search-modal-button").click();
+  }
+});
