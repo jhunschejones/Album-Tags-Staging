ef95891b735854c20ea3ffbaab3afe962f11b61d
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-02-08 17:35:11 -0800

    Remove jquery scrolling plugin
    Scrolling works as intended without plugin

--- public/javascripts/myfavorites.script.js
+++ public/javascripts/myfavorites.script.js
@@ -366,18 +366,6 @@ $(document).on( 'scroll', function(){
   $('#artist_filter_menu').removeClass('show');
 });
 
-// $(function() {
-//   var toolbox = $('.dropdown-menu'),
-//     height = toolbox.height(),
-//     scrollHeight = toolbox.get(0).scrollHeight;
-
-//   toolbox.bind('mousewheel', function(e, d) {
-//     if((this.scrollTop === (scrollHeight - height) && d < 0) || (this.scrollTop === 0 && d > 0)) {
-//       e.preventDefault();
-//     }
-//   });
-// });
-
 // filters artists in filter to match what albums are on page
 function whatsOnThePage_artists() {
   let whatsLeft = $('.albumCard:visible')
