6455698d362dc33be5f67a04a8377e789f5735ee
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-02-26 07:56:16 -0800

    Retire search and favorites pages, update routes accordingly

--- routes/static.routes.js
+++ routes/static.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const newrelic = require('newrelic')
 
 // GET home page
 router.get('/', function(req, res, next) {
@@ -19,13 +20,15 @@ router.get('/about', function(req, res, next) {
   })
 })
 
-// GET album details page
+// GET legacy album details page
+// Deprecated winter 2018, redirecting to new `album` page
 router.get('/albumdetails/:albumId', function(req, res, next) {
   // res.render('albumdetails', {
   //   pageTitle: ': Details',
   //   subTitle: 'Album Details',
   //   albumId: req.params.albumId
   // })
+  newrelic.recordCustomEvent('DeprecatedPageCalled', {'call': 'GET', 'page': req.url});
   res.redirect('/album/' + req.params.albumId)
 })
 
@@ -45,13 +48,22 @@ router.get('/alltags', function(req, res, next) {
   })
 })
 
-// GET our favorites page
+// GET legacy our-favorites page
+// Deprecated 02.25.19, redirecting to `myfavorites` page
 router.get('/favorites', function(req, res, next) {
-  res.setHeader("Cache-Control", "private, max-age=600")
-  res.render('favorites', { 
-    pageTitle: `: ${(new Date()).getFullYear()}`,
-    subTitle: `Our Favorite Albums of ${(new Date()).getFullYear()}`
-  })
+  // res.setHeader("Cache-Control", "private, max-age=600")
+  // res.render('favorites', { 
+  //   pageTitle: `: ${(new Date()).getFullYear()}`,
+  //   subTitle: `Our Favorite Albums of ${(new Date()).getFullYear()}`
+  // })
+  newrelic.recordCustomEvent('DeprecatedPageCalled', {'call': 'GET', 'page': req.url});
+  res.redirect('/myfavorites'); 
+})
+
+// GET custom link to Josh's favorites as a list
+router.get('/josh', function(req, res, next) {
+  newrelic.recordCustomEvent('JoshFavoritesCalled', {'call': 'GET', 'page': req.url});
+  res.redirect(`/list?type=favorites&id=07ade7507501f3c242640832f57a323a9ca66c70b6496d0ff1b2262a9f12c83a8b45023f4eda83826b5d43726a16e7f9f50d9d60d37459f57a&year=${(new Date()).getFullYear()}`); 
 })
 
 // GET "my favorites" page
@@ -63,13 +75,16 @@ router.get('/myfavorites', function(req, res, next) {
   })
 })
 
-// GET search page
+// GET legacy search page
+// Deprecated 02.25.19, redirecting to `home` page
 router.get('/search', function(req, res, next) {
-  res.setHeader("Cache-Control", "private, max-age=600")
-  res.render('search', {
-    pageTitle: ': Search',
-    subTitle: 'Search By Album or Artist'
-  })
+  // res.setHeader("Cache-Control", "private, max-age=600")
+  // res.render('search', {
+  //   pageTitle: ': Search',
+  //   subTitle: 'Search By Album or Artist'
+  // })
+  newrelic.recordCustomEvent('DeprecatedPageCalled', {'call': 'GET', 'page': req.url});
+  res.redirect('/'); 
 })
 
 // GET tags-search page
@@ -81,13 +96,15 @@ router.get('/search/tags/:selectedtags', function(req, res, next) {
   })
 })
 
-// GET album update page
+// GET legacy album update page
+// Deprecated winter 2018, redirecting to new `album` page
 router.get('/update/:albumId', function(req, res) {
   // res.render('update', {
   //   pageTitle: ': Update',
   //   subTitle: 'Update Album Tags',
   //   albumId: req.params.albumId
   // })
+  newrelic.recordCustomEvent('DeprecatedPageCalled', {'call': 'GET', 'page': req.url});
   res.redirect('/album/' + req.params.albumId)
 })
 
