e162502a4a77802defcd95beb2279762343f0a20
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-03-03 09:33:17 -0800

    Remove utility routes, re-enable endpoints for testing

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -7,21 +7,20 @@ const album_controller = require('../controllers/album.controller')
 // Album CRUD functionality
 router.get('/', album_controller.return_all_albums)
 router.post('/', album_controller.add_new_album)
-
 // Deprecated 02.20.19, redirecting to find by apple album ID endpoint
-// router.get('/:id', album_controller.get_album_details) // by database _id
-router.get('/:id', function(req, res) { 
-  newrelic.recordCustomEvent('DeprecatedEndpointCalled', {'call': 'GET', 'endpoint': `/api/v1/album${req.url}`})
-  res.redirect('/api/v1/album/albumid/' + req.params.id); 
-})
-
+// Turned back on 03.02.19 for tests 
+router.get('/:id', album_controller.get_album_details) // by database _id
+// router.get('/:id', function(req, res) { 
+//   newrelic.recordCustomEvent('DeprecatedEndpointCalled', {'call': 'GET', 'endpoint': `/api/v1/album${req.url}`})
+//   res.redirect('/api/v1/album/albumid/' + req.params.id); 
+// })
 // Deprecated 02.20.19, replacing with warning message
-// router.put('/:id', album_controller.update_entire_album)
-router.put('/:id', function(req, res) {
-  newrelic.recordCustomEvent('DeprecatedEndpointCalled', {'call': 'PUT', 'endpoint': `/api/v1/album${req.url}`})
-  res.send({ "message": "the full album update endpoint was deprecated on 02.20.19" })
-})
-
+// Turned back on 03.02.19 for database error corrections 
+router.put('/:id', album_controller.update_entire_album)
+// router.put('/:id', function(req, res) {
+//   newrelic.recordCustomEvent('DeprecatedEndpointCalled', {'call': 'PUT', 'endpoint': `/api/v1/album${req.url}`})
+//   res.send({ "message": "the full album update endpoint was deprecated on 02.20.19" })
+// })
 router.delete('/:id', album_controller.delete_album)
 router.get('/albumid/:id', album_controller.find_by_apple_album_id) // by appleAlbumID
 // connections and tags
@@ -34,9 +33,5 @@ router.delete('/connections/:id', album_controller.delete_connection)
 router.get('/favorites/:user', album_controller.get_favorites)
 router.post('/favorites/:id', album_controller.add_favorite) // by database _id
 router.delete('/favorites/:id', album_controller.delete_favorite) // by database _id
-// utility page calls
-router.get('/utility/missingtags', album_controller.find_albums_missing_tags)
-router.get('/utility/blankalbums', album_controller.find_blank_albums)
-router.get('/utility/duplicates', album_controller.find_duplicate_apple_album_id)
 
 module.exports = router
