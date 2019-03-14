ee6aed5ac8731028bfee86abd71e0ecfe48563ca
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-03-14 07:47:29 -0700

    Add song order to song model

--- models/album_song.model.js
+++ models/album_song.model.js
@@ -3,6 +3,7 @@ const sequelize = require('../sequelize.js');
 
 const Song = sequelize.define('song', {
   name: { type: Sequelize.STRING, allowNull: false },
+  order: { type: Sequelize.INTEGER, allowNull: false },
   appleAlbumID: { type: Sequelize.INTEGER, allowNull: false }
 });
 
