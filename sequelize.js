988f474073499a5a95fcb0cc470211973493d1db
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-03-28 11:16:53 -0700

    Include code for connecting to GCP MySQL and SQLite

--- sequelize.js
+++ sequelize.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+// ------ SQLite Local Database ------
 // const sequelize = new Sequelize('database', 'username', 'password', {
 //   dialect: 'sqlite',
 //   storage: 'data/db/dev.sqlite',
@@ -6,7 +7,16 @@ const Sequelize = require('sequelize');
 //   logging: false
 // });
 
-// http://docs.sequelizejs.com/manual/usage.html
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Sqlite connection established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// ------ MySQL GCP Database ------
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   dialect: 'mysql',
   host: process.env.DB_HOST,
