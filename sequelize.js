b563cc50896ea6f15acea086f2bbffb3ff7a8ec2
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-04-04 09:48:33 -0700

    Automatically connect to prod or dev databases based on environment
    Instead of commenting and un-commenting code, this logic branching allows the app to connect to the propper database for the environment it is running in

--- sequelize.js
+++ sequelize.js
@@ -1,36 +1,41 @@
 const Sequelize = require('sequelize');
-// ------ SQLite Local Database ------
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   dialect: 'sqlite',
-//   storage: 'data/db/dev.sqlite',
-//   operatorsAliases: false,
-//   logging: false
-// });
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Sqlite connection established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+if (process.env.NODE_ENV === "development") {
+  // ------ SQLite Local Database ------
+  const sequelize = new Sequelize('database', 'username', 'password', {
+    dialect: 'sqlite',
+    storage: 'data/db/dev.sqlite',
+    operatorsAliases: false,
+    logging: false
+  });
+  
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Sqlite connection established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the Sqlite database:', err);
+    });
 
-// ------ MySQL GCP Database ------
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  operatorsAliases: false,
-  logging: false
-});
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('MySQL connection established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  module.exports = sequelize;
+} else {
+  // ------ MySQL GCP Database ------
+  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    operatorsAliases: false,
+    logging: false
   });
 
-module.exports = sequelize;
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('MySQL connection established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the MySQL database:', err);
+    });
+
+  module.exports = sequelize;
+}
