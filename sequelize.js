044d3e14d49329ba0dba9b82f2770eee03de57fa
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-05-03 19:22:18 -0700

    Remove operatorAliases: false & update nodemon

--- sequelize.js
+++ sequelize.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "development") {
   const sequelize = new Sequelize('database', 'username', 'password', {
     dialect: 'sqlite',
     storage: 'data/db/dev.sqlite',
-    operatorsAliases: false,
+    // operatorsAliases: false,
     logging: false
   });
   
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "development") {
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     dialect: 'mysql',
     host: process.env.DB_HOST,
-    operatorsAliases: false,
+    // operatorsAliases: false,
     logging: false
   });
 
